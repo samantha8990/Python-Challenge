bf0ff28b92b4788d578efaec6fc14a2b8e1753c5
Samantha Sommer <samanthajeddy@gmail.com>
2021-03-28 20:27:27 -0700

    hw

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import statistics
 
 #get file path
 pybank_path= os.path.join('PyBank','Resources','pybank.csv')
@@ -12,13 +13,15 @@ with open(pybank_path) as csv_file:
 #set variables
     months=[]
     profit=[]
-    #maxprofit=[]
-    #minprofit=[]
-
+    #max_profit=[]
+    #min_profit=[]
+    #value_sum=0
     for row in csv_reader:
         months.append(row[0])
-        profit.append(int(row[1]))
+        #profit.append(int(row[1]))-this add all of column profit/losses
+        #value_sum += int(row[1])- this add all of column profit/losses
 
+        #calculate max profit
         #if
 
 
@@ -27,6 +30,10 @@ with open(pybank_path) as csv_file:
     print("---------------------------")
     print("Total Months:", len(months))
     print("Total:", sum(profit))
+    #print("Average Change:",) can use statistics.mean?
+    #print("Greatest Increase in Profits:")
+    #print("Greatest Decrease in Profits:")
+
        
   
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,2 +1,28 @@
-print("Hello")
-print("World")
+import os
+import csv
+
+#get file path
+pybank_path= os.path.join('PyPoll','Resources','pypoll.csv')
+
+#open file
+with open(pybank_path) as csv_file:
+    csv_reader=csv.reader(csv_file, delimiter=',')
+    csv_reader=next(csv_file)
+
+#set variables
+ votes= []
+ candidates=[]
+ candidatevotes=[]
+
+ totalvote=0
+
+    for row in csv_reader:
+        votes.appen(row[2])
+totalvote=len(votes)
+
+
+
+    print("Election Results")
+    print("---------------------------")
+    print("Total Votes:", len(votes))
+    print("Total:", sum(profit))
