fd67251f72f2773a51332e77eb1b8f6c71e255ed
Samantha Sommer <samanthajeddy@gmail.com>
2021-03-28 21:00:02 -0700

    hw

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,19 +10,30 @@ with open(pybank_path) as csv_file:
     csv_reader=next(csv_file)
 
 #set variables
- votes= []
- candidates=[]
- candidatevotes=[]
+ #votes= []
+ #candidates=[]
+ #candidatevotes=[]
 
- totalvote=0
+    total_vote=0
 
-    for row in csv_reader:
-        votes.appen(row[2])
-totalvote=len(votes)
+    for column in csv_reader:
+        total_vote += 1
+       
+    #candidates and votes they got
+    #Khan=int(candidates.count("Khan"))
+    #Correy=int(candidates.count("Correy"))
+    #Li=int(candidates.count("Li"))
+    #O'Tooley=int(candidates.count("O'Tooley"))
 
+    #% of total vote for each canidate
+    #Khan_per=(Khan/total_vote)*100
+    #Correy_per=(Correy/total_vote)*100
+    #Li_per=(Li/total_vote)*100
+    #O'Tooley_per=(O'Tooley/total_vote)*100
 
 
     print("Election Results")
     print("---------------------------")
-    print("Total Votes:", len(votes))
-    print("Total:", sum(profit))
+    print("Total Votes:", total_vote)
+    #print("Khan:", khan_per Khan)
+    
